30d6cbf12506fbf26055e6d73978335c82d084be
Giocodingtemple <gmarchiori5149@gmail.com>
2022-07-08 14:29:58 -0500

    first commit

--- modforexercises.py
+++ modforexercises.py
@@ -1,1 +1,14 @@
 from math import pi
+#Rectangles       A = length * wwidth
+def rectangle():
+    length = int(input("Enter the side:"))
+    width = int(input("Enter the side:"))
+    Area = length * width
+    print("Area of the Rectangle="+str(Area))
+
+
+#Circles          A = pi * r * 2
+def circle():
+    r = int(input("Enter the side:"))
+    Area = pi * r * 2
+    print("Area of the Circle="+str(Area))
